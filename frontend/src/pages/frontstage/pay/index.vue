a2e57067160f4782d3c8c916abc3cdfb42755383
mlhiter <3076438032@qq.com>
2023-04-15 10:18:40 +0800

    feat: 缴费页面

--- frontend/src/pages/frontstage/pay/index.vue
+++ frontend/src/pages/frontstage/pay/index.vue
@@ -1,9 +1,63 @@
 <template>
-  <div>我是缴费界面</div>
+  <n-card title="缴费账单">
+    <template #header-extra>{{ check.paymentTime }}</template>
+    账单id:{{ check.ID }}
+    <br />
+    学生id:{{ check.studentId }}
+    <br />
+    学生姓名:{{ check.studentName }}
+    <br />
+    课程id:{{ check.courseId }}
+    <br />
+    课程名称:{{ check.courseName }}
+    <br />
+    课程费用:{{ check.paymentAmount }}
+    <br />
+    收款人:{{ check.payee }}
+    <br />
+    收款账户:{{ check.payAccount }}
+    <br />
+    <template #action>
+      <n-button attr-type="button" type="success" @click="handlePayment">
+        缴费
+      </n-button>
+    </template>
+  </n-card>
 </template>
 
-<script lang='ts' setup>
-import {} from 'vue'
+<script lang="ts" setup>
+import { useMessage } from 'naive-ui'
+const message = useMessage()
+const check = ref({
+  ID: 1,
+  studentId: 1,
+  studentName: '李晨洋',
+  courseId: 8,
+  courseName: 'C语言',
+  paymentAmount: 10000,
+  payee: '李致知',
+  payAccount: '6222031614003722522',
+  paymentTime: '2023-04-14 21:06:23',
+})
+const studentName = '李致知'
+const fetchCheckList = async () => {
+  try {
+    const res = await axios.get('/api/pay/individual', {
+      data: { studentName: studentName },
+    })
+    let tempCheck = res.data.data[0]
+    check.value = { ...check.value, ...tempCheck }
+    console.log(check.value)
+  } catch (error) {
+    console.log(error)
+  }
+}
+function handlePayment() {
+  message.success('缴费成功!')
+}
+onActivated(async () => {
+  fetchCheckList()
+})
 </script>
 
-<style lang='sass' scoped></style>
+<style lang="sass" scoped></style>
